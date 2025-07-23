c8639033d5d422cb819ab858949c79d734a3834f
Pratyush <pratyushsinghrajawat@gmail.com>
2025-07-23 23:43:04 +0530

    Style changes in Navbar and darkmode

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,10 +8,12 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import DarkMode from '../DarkMode'
 
 
 const Navbar = () => {
-    const user = false;
+    const user = true;
     return (
         <div className='h-16 dark:bg-[#0A0A0A] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10'>
             {/* DeskTop */}
@@ -21,17 +23,23 @@ const Navbar = () => {
                     <h1 className='hidden md:block font-extrabold text-2xl'>E-Learning</h1>
                 </div>
                 {/* User Icon & Dark Mode Icon */}
-                <div>
+                <div className='flex items-center gap-5'>
                     {
                         user ? (<DropdownMenu>
-                            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+                            <DropdownMenuTrigger>
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                            </DropdownMenuTrigger>
                             <DropdownMenuContent>
                                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
                                 <DropdownMenuSeparator />
-                                <DropdownMenuItem>Profile</DropdownMenuItem>
-                                <DropdownMenuItem>Billing</DropdownMenuItem>
-                                <DropdownMenuItem>Team</DropdownMenuItem>
-                                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                                <DropdownMenuItem>My Learning</DropdownMenuItem>
+                                <DropdownMenuItem>Edit Profile</DropdownMenuItem>
+                                <DropdownMenuItem>Log Out</DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>Dashboard</DropdownMenuItem>
                             </DropdownMenuContent>
                         </DropdownMenu>) : (
                             <div className='flex items-center gap-2'>
@@ -40,6 +48,7 @@ const Navbar = () => {
                             </div>
                         )
                     }
+                    <DarkMode />
                 </div>
             </div>
         </div>
